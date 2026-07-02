ebebf0eec7e343a045677c3b3207613ad2e9af42
Anish Rana <anish.rana.work@gmail.com>
2026-07-02 20:56:59 +0530

    Time: 3 ms (53.72%), Space: 12.1 MB (11.11%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int i;
-        unordered_map<char,int> r;
-        unordered_map<char,int> z;
-        int n=ransomNote.size();
-        int m=magazine.size();
-        for(i=0;i<n;i++){
-            r[ransomNote[i]]++;
-        }
-        for(i=0;i<m;i++){
-            z[magazine[i]]++;
-        }
-        for(i = 0; i < n; i++) {
-            if(r[ransomNote[i]] > z[ransomNote[i]])
-            return false;
-        }
-        return true;
+        unordered_map<char,int> have;
+        unordered_map<char,int> need;
 
+        int n = ransomNote.size();
+        int m = magazine.size();
+
+        for(i = 0; i < n; i++){
+            need[ransomNote[i]]++;
+        }
+
+        for(i = 0; i < m; i++){
+            have[magazine[i]]++;
+        }
+
+        for(auto i : need){
+            char c = i.first;
+            int fNeed = i.second;
+            int fHave = have[c];
+
+            if(fHave < fNeed)
+                return false;
+        }
+
+        return true;
     }
 };
