bf449e398c8076fbb33c8bfa7f5bf9bfc7f81d12
Anish Rana <anish.rana.work@gmail.com>
2026-07-11 20:06:38 +0530

    Time: 8 ms (55.21%), Space: 22.8 MB (80.6%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
-
     long long fun(vector<int> &a, int n, int speed)
     {
-        long long h = 0;
-
-        for (int i = 0; i < n; i++)
+        long long h=0;
+        for (int i=0; i<n;i++)
         {
-            h += a[i] / speed;
-
-            if (a[i] % speed != 0)
+            h += a[i]/speed;
+            if(a[i]%speed!=0)
                 h++;
         }
-
         return h;
     }
-
     int minEatingSpeed(vector<int> &a, int guard)
     {
-        int n = a.size();
-
-        int low = 1;
-        int high = *max_element(a.begin(), a.end());
-
-        int res = -1;
-
-        while (low <= high)
+        int n=a.size();
+        int low=1;
+        int high=*max_element(a.begin(), a.end());
+        int res=-1;
+        while(low<=high)
         {
-            int guess = low + (high - low) / 2;
-
-            long long hour = fun(a, n, guess);
-
-            if (hour > guard)
+            int guess=low+(high-low)/2;
+            long long hour=fun(a,n,guess);
+            if (hour>guard)
             {
-                low = guess + 1;
+                low=guess+1;
             }
             else
             {
-                res = guess;
-                high = guess - 1;
+                res=guess;
+                high=guess-1;
             }
         }
-
         return res;
     }
 };
