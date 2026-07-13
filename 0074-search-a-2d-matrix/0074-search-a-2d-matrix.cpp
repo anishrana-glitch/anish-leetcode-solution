8b58a3e7b3c0306c2f4704ffe7f20f8b19c51ae7
Anish Rana <anish.rana.work@gmail.com>
2026-07-13 06:41:53 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,32 +5,18 @@ public:
             return false;
         int m = matrix.size();
         int n = matrix[0].size();
-        int low = 0;
-        int high = m - 1;
-        int res = -1;
-        // Find the correct row
-        while (low <= high) {
-            int guess = low + (high - low) / 2;
-            if (matrix[guess][0] <= target) {
-                res = guess;
-                low = guess + 1;
-            } else {
-                high = guess - 1;
-            }
-        }
-        if (res == -1)
-            return false;
-        int LOW = 0;
-        int HIGH = n - 1;
-        // Search within the row
-        while (LOW <= HIGH) {
-            int guess = LOW + (HIGH - LOW) / 2;
-            if (matrix[res][guess] == target)
-                return true;
-            else if (matrix[res][guess] < target)
-                LOW = guess + 1;
+        int low=0;
+        int high=(m*n)-1;
+        while(low<=high){
+            int guess=low+(high-low)/2;
+            int row=guess/n;
+            int col=guess%n;
+            if(matrix[row][col]==target)
+            return true;
+            if(matrix[row][col]<target)
+            low=guess+1;
             else
-                HIGH = guess - 1;
+            high=guess-1;
         }
         return false;
     }
