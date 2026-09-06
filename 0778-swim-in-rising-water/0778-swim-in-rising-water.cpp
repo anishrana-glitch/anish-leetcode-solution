5229d2989fc1c56d1ed6c49bde156c24e5f769cd
Anish Rana <anish.rana.work@gmail.com>
2026-09-06 19:35:24 +0530

    Time: 3 ms (87.7%), Space: 12.8 MB (74.56%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -7,71 +7,54 @@ public:
         return true;
     }
 
-    bool bfs(vector<vector<int>> &a, int n, int m, int money) {
-        int x[4] = {1, -1, 0, 0};
-        int y[4] = {0, 0, 1, -1};
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-        queue<pair<int,int>> q;
+        vector<vector<int>> res(n, vector<int>(m, INT_MAX));
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<pair<int, pair<int,int>>>
+        > pq;
 
-        q.push({0, 0});
-        vis[0][0] = 1;
+        int x[] = {-1, 1, 0, 0};
+        int y[] = {0, 0, 1, -1};
 
-        while(!q.empty()) {
-            pair<int,int> p = q.front();
-            q.pop();
+        res[0][0] = grid[0][0];
+        pq.push({grid[0][0], {0, 0}});
 
-            int row = p.first;
-            int col = p.second;
+        while(!pq.empty()) {
+            pair<int, pair<int,int>> p = pq.top();
+            pq.pop();
+
+            int dis = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            if(dis > res[row][col])
+                continue;
 
             if(row == n-1 && col == m-1)
-                return true;
+                return dis;
 
             for(int k = 0; k < 4; k++) {
                 int r = row + x[k];
                 int c = col + y[k];
 
-                if(valid(r, c, n, m) &&
-                   vis[r][c] == 0 &&
-                   money >= a[r][c]) {
+                if(!valid(r, c, n, m))
+                    continue;
 
-                    q.push({r, c});
-                    vis[r][c] = 1;
+                int newwt = max(dis, grid[r][c]);
+
+                if(newwt < res[r][c]) {
+                    res[r][c] = newwt;
+                    pq.push({newwt, {r, c}});
                 }
             }
         }
 
-        return false;
-    }
-
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int low = grid[0][0];
-        int high = grid[0][0];
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                high = max(high, grid[i][j]);
-            }
-        }
-
-        int res = high;
-
-        while(low <= high) {
-            int guess = (low + high) / 2;
-
-            if(bfs(grid, n, m, guess)) {
-                res = guess;
-                high = guess - 1;
-            }
-            else {
-                low = guess + 1;
-            }
-        }
-
-        return res;
+        return res[n-1][m-1];
     }
 };
