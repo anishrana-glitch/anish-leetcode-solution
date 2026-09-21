f7d26107f3ca7465d1cad36c539201a9f8369b41
Anish Rana <anish.rana.work@gmail.com>
2026-09-21 18:59:50 +0530

    Time: 2 ms (77.58%), Space: 7.9 MB (37.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -5,12 +5,14 @@ public:
        return 0;
        if (n==1)
        return 1;
-       vector<int> dp(n+1);
-       dp[0]=0;
-       dp[1]=1;
+       int prev =1;
+       int prev_prev =0;
+       int ans = 0;
        for(int i=2; i<=n;i++){
-        dp[i]=dp[i-1] + dp[i-2];
+        ans = prev+prev_prev;
+        prev_prev = prev;
+        prev = ans;
        }
-       return dp[n];
+       return ans;
     }
 };
