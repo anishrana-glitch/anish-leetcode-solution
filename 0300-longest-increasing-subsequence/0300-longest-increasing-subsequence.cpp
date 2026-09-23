e280ccc2758a92334b4fc140e1629064e546c297
Anish Rana <anish.rana.work@gmail.com>
2026-09-23 22:05:03 +0530

    Time: 0 ms (100%), Space: 14.2 MB (78.06%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis;
+        lis.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); ++i) {
+            if (lis.back() < nums[i]) {
+                lis.push_back(nums[i]);
+            } else {
+                int j = lower_bound(lis.begin(), lis.end(), nums[i]) - lis.begin();
+                lis[j] = nums[i];
+            }
+        }
+        return lis.size();
+    }
+};
